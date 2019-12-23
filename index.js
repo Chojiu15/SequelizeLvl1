7c019624851820fa9f9ce7521e51a3c50200ff5e
chojiu <yacine@wildcodeschool.fr>
2019-12-23 16:02:04 +0100

    models User and Task created / data.js created(used for injecting data into User) / index.js completed to run the script by nodemin index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const models = require('./models')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
+
+
+models
+    .sequelize
+    .sync({force : true})
+    .then(() =>{
+        app.listen(3000, console.log('Server is running well'))
+    })
